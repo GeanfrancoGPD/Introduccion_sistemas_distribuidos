acd9a7d057747289dda3bee1d20950ce61681e40
Geanfranco <geanfranco1212@gmail.com>
2026-02-10 19:42:26 -0400

    feat: Se agregaron los metodos de serealizado y deserealizado

--- tarea1/cliente.js
+++ tarea1/cliente.js
@@ -3,7 +3,7 @@ import ProxyCalculadora from "./servidores/proxyCalculadora.js";
 async function main() {
   const cli = new ProxyCalculadora();
 
-  const sum = await cli.sumar(5, 3);
+  const sum = await cli.sumar(17, 3);
   console.log("Resultado de sumar:", sum);
 
   const res = await cli.restar(5, 3);
--- tarea1/servidores/clientConnect.js
+++ tarea1/servidores/clientConnect.js
@@ -5,19 +5,27 @@ export default class ClientConnect {
     this.socket = new WebSocket("ws://localhost:5000");
   }
 
+  serialize(method, params) {
+    return JSON.stringify({ method, params });
+  }
+
+  deserialize(buffer) {
+    return JSON.parse(buffer.toString());
+  }
+
   send(method, params) {
     return new Promise((resolve, reject) => {
       try {
         if (this.socket.readyState === WebSocket.OPEN) {
-          this.socket.send(JSON.stringify({ method, params }));
+          this.socket.send(this.serialize(method, params));
         } else {
           this.socket.once("open", () => {
-            this.socket.send(JSON.stringify({ method, params }));
+            this.socket.send(this.serialize(method, params));
           });
         }
 
         return this.socket.once("message", (message) => {
-          const res = JSON.parse(message);
+          const res = this.deserialize(message);
           resolve(res.result);
         });
       } catch (err) {
